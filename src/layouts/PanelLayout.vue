3a9c5190b916c5e14ad60be169cb1d546d7f8c98
Daniel <danielkasperczyk@gmail.com>
2022-08-13 15:15:14 +0200

    application start

---
+++ src/layouts/PanelLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <slot name="header" />
+  </header>
+  <main class="content">
+    <aside class="content__left">
+      <slot name="left-panel" />
+    </aside>
+    <div class="content__main">
+      <slot name="main" />
+    </div>
+    <aside class="content__right">
+      <slot name="right-panel" />
+    </aside>
+  </main>
+</template>
+
+<script lang="ts" setup></script>
+<style lang="scss" scoped>
+$header-height: 32px;
+$panel-width: 160px;
+
+.header {
+  height: $header-height;
+}
+.content {
+  display: flex;
+  height: calc(100vh - $header-height);
+  &__left,
+  &__right {
+    width: 100%;
+    max-width: $panel-width;
+  }
+  &__main {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <PanelLayout>
+    <template #header> header </template>
+    <template #left-panel> left panel </template>
+    <template #main>
+      <div class="test">test</div>
+    </template>
+    <template #right-panel> left panel </template>
+  </PanelLayout>
+</template>
+
+<script setup lang="ts">
+import PanelLayout from "@/layouts/PanelLayout.vue";
+</script>
+
+<style lang="scss" scoped></style>
